feb5729d17de245b7dadc6e9a0d803035b0ab46c
beccyo3o <rebecca.krummel@gmail.com>
2017-08-29 11:26:32 +0200

    db query set up

---
+++ database.js
@@ -0,0 +1,24 @@
+//Spiced Academy module which sets up client to talk to DB from node.js
+const spicedPg = require('spiced-pg');
+
+//get database
+const db = spicedPg(require('./secrets').db);
+// const secrets = req uire('./secrets.json');
+// const db = spicedPg(`postgres:${secrets.dbUser}:${secrets.pass}@localhost:5432/images`);
+
+// get images
+var displayImages = function(){
+    const queryText = 'SELECT * FROM images';
+
+    db.query(queryText).then((result)=>{
+        console.log("Images:" + result.rows);
+        return result.rows;
+    }).catch((err)=>{
+        console.log(err);
+    });
+};
+
+
+module.exports = {
+    displayImages
+};
