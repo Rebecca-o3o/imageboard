47942cf4277a60ab8cf6f10ef8ec7917eb0b8fd3
beccyo3o <rebecca.krummel@gmail.com>
2017-08-30 00:16:25 +0200

    add uplad model and view

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,7 @@
     Handlebars.partials = Handlebars.templates;
 
 
-    //BACKBONE MODEL
+    //------ BACKBONE MODELS ------ //
     var HomeModel = Backbone.Model.extend({
         initialize: function() {
             this.fetch();
@@ -20,7 +20,40 @@
     });
     var homeModel = new HomeModel;
 
-    //BACKBONE VIEW
+
+    var UploadModel = Backbone.Model.extend({
+        initialize: function() {
+            this.fetch();
+        },
+        url: '/upload',
+        save: function() {              //overwrite save function
+            //get first file used in html form input:
+            var file = $('input[type="file"]').get(0).files[0];
+
+            //create FormData:
+            var formData = new FormData;
+
+            //attach file and img title to formData:
+            formData.append('file', file);
+            formData.append('title', this.get('title'));
+
+            //send FormData in ajax POST request:
+            var model = this;
+            $.ajax({
+                url: this.url,
+                method: 'POST',
+                data: formData,
+                processData: false,         //prevent jQuerys usuall process
+                contentType: false,         //prevent jQuerys usuall process
+                success: function() {
+                    model.trigger("upload Success!");
+                }
+            });
+        }
+    });
+
+
+    // ------ BACKBONE VIEWS ------ //
     var HomeView = Backbone.View.extend({
         initialize: function() {
             var view = this;
@@ -37,19 +70,53 @@
         }
     });
 
-    //Route handlers
+
+    var UploadView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            //ref to .html template id=upload
+            this.$el.html(Handlebars.templates.upload({}));
+        },
+        events: {
+            'click button': function(e) {
+                this.model.set({
+                    title: this.$el.find('input[name="title"]').val(),
+                    file: this.$el.find('input[type="file"]').prop('files')[0]
+                }).save();                  //overwritten save function
+            }
+        }
+    });
+
+    // ------ Route handlers ------ //
     var homeView = new HomeView({
         model: new HomeModel(),
         el: '#main'
     });
+    // var uploadView = new UploadView({
+    //     model: new UploadModel(),
+    //     el: '#main'
+    // });
 
-    //BACKBONE ROUTER
+    // ------ BACKBONE ROUTER ------ //
     var Router = Backbone.Router.extend({
         routes:{
-            '*': 'home'
+            // 'image/:id': 'image',
+            'upload': 'upload',
+            '': 'home'
         },
         home: function(){
             homeView.render();
+        },
+        // upload: function() {
+        //     uploadView.render();
+        // }
+        upload: function() {
+            new UploadView({
+                el: '#main',
+                model: new UploadModel
+            });
         }
     });
 
