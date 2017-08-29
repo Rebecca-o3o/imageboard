41dc99565e85025c8e7aa6068c4e017683c59b11
beccyo3o <rebecca.krummel@gmail.com>
2017-08-29 23:09:34 +0200

    add app.post file-uploader

--- index.js
+++ index.js
@@ -5,25 +5,27 @@ const {s3Url} = require('./config.json');
 const dbQuery = require('./database');
 
 //modules for img upload
-const multer = require('multer');
+const multer = require('multer');           // does chunks of huge files
 const uidSafe = require('uid-safe');
 const path = require('path');
 
 var diskStorage = multer.diskStorage({
+    // determining the path and filename to use when saving files
     destination: function (req, file, callback) {
         callback(null, __dirname + '/uploads');
     },
     filename: function (req, file, callback) {
-      uidSafe(24).then(function(uid) {
-          callback(null, uid + path.extname(file.originalname));
-      });
+        //use unique id as file name with original extension
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
     }
 });
 
 var uploader = multer({
     storage: diskStorage,
     limits: {
-        filesize: 2097152       //always set a limit!
+        filesize: 2097152       //(ca. 2MB) set a limit to prevent DOS attacks
     }
 });
 
@@ -52,5 +54,18 @@ app.get('/home', function(req, res){
     });
 });
 
+
+app.post('/upload', uploader.single('file'), function(req, res) {
+    console.log(req.file);
+    if (req.file) {
+        res.json({
+            success: true
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
+});
 
 app.listen(8080, ()=> console.log('Server listening on port 8080'));
