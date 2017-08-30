c3701f30333ecd87d9905b68b15a54694b3a6c31
beccyo3o <rebecca.krummel@gmail.com>
2017-08-30 08:51:58 +0200

    upload view available

--- public/js/script.js
+++ public/js/script.js
@@ -26,32 +26,32 @@
             this.fetch();
         },
         url: '/upload',
-        save: function() {              //overwrite save function
-            //get first file used in html form input:
-            var file = $('input[type="file"]').get(0).files[0];
-
-            //create FormData:
-            var formData = new FormData;
-
-            //attach file and img title to formData:
-            formData.append('file', file);
-            formData.append('title', this.get('title'));
-
-            //send FormData in ajax POST request:
-            var model = this;
-            $.ajax({
-                url: this.url,
-                method: 'POST',
-                data: formData,
-                processData: false,         //prevent jQuerys usuall process
-                contentType: false,         //prevent jQuerys usuall process
-                success: function() {
-                    model.trigger("upload Success!");
-                }
-            });
-        }
+        // save: function() {              //overwrite save function
+        //     //get first file used in html form input:
+        //     var file = $('input[type="file"]').get(0).files[0];
+        //
+        //     //create FormData:
+        //     var formData = new FormData;
+        //
+        //     //attach file and img title to formData:
+        //     formData.append('file', file);
+        //     formData.append('title', this.get('title'));
+        //
+        //     //send FormData in ajax POST request:
+        //     var model = this;
+        //     $.ajax({
+        //         url: this.url,
+        //         method: 'POST',
+        //         data: formData,
+        //         processData: false,         //prevent jQuerys usuall process
+        //         contentType: false,         //prevent jQuerys usuall process
+        //         success: function() {
+        //             model.trigger("upload Success!");
+        //         }
+        //     });
+        // }
     });
-
+    var uploadModel = new UploadModel;
 
     // ------ BACKBONE VIEWS ------ //
     var HomeView = Backbone.View.extend({
@@ -79,14 +79,14 @@
             //ref to .html template id=upload
             this.$el.html(Handlebars.templates.upload({}));
         },
-        events: {
-            'click button': function(e) {
-                this.model.set({
-                    title: this.$el.find('input[name="title"]').val(),
-                    file: this.$el.find('input[type="file"]').prop('files')[0]
-                }).save();                  //overwritten save function
-            }
-        }
+        // events: {
+        //     'click button': function(e) {
+        //         this.model.set({
+        //             title: this.$el.find('input[name="title"]').val(),
+        //             file: this.$el.find('input[type="file"]').prop('files')[0]
+        //         }).save();                  //overwritten save function
+        //     }
+        // }
     });
 
     // ------ Route handlers ------ //
@@ -94,10 +94,10 @@
         model: new HomeModel(),
         el: '#main'
     });
-    // var uploadView = new UploadView({
-    //     model: new UploadModel(),
-    //     el: '#main'
-    // });
+    var uploadView = new UploadView({
+        model: new UploadModel(),
+        el: '#main'
+    });
 
     // ------ BACKBONE ROUTER ------ //
     var Router = Backbone.Router.extend({
@@ -109,15 +109,15 @@
         home: function(){
             homeView.render();
         },
-        // upload: function() {
-        //     uploadView.render();
-        // }
         upload: function() {
-            new UploadView({
-                el: '#main',
-                model: new UploadModel
-            });
+            uploadView.render();
         }
+        // upload: function() {
+        //     new UploadView({
+        //         el: '#main',
+        //         model: new UploadModel
+        //     });
+        // }
     });
 
     var router = new Router;
